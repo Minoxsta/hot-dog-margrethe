59863d35824c4cd04abdf8c44806170f24a07f20
Esben10 <esbenmoldt@gmail.com>
2025-01-14 19:33:50 +0100

    idkkkk

--- scenes/pølse/pølse.gd
+++ scenes/pølse/pølse.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	$Sprite2D.position = get_viewport().get_mouse_position()
